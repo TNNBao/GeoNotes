e1b9d1dc1539706707dae5a1de7e1ac0c5121cbd
Bảo Trần <baotnn.22git@vku.udn.vn>
2025-10-04 22:11:25 +0700

    fix map marker

--- src/components/GeoNotes.tsx
+++ src/components/GeoNotes.tsx
@@ -3,6 +3,23 @@ import L, { Map as LeafletMap } from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { useGeoNotes } from "../hooks/useGeoNotes";
 
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: new URL(
+    "leaflet/dist/images/marker-icon-2x.png",
+    import.meta.url
+  ).toString(),
+  iconUrl: new URL(
+    "leaflet/dist/images/marker-icon.png",
+    import.meta.url
+  ).toString(),
+  shadowUrl: new URL(
+    "leaflet/dist/images/marker-shadow.png",
+    import.meta.url
+  ).toString(),
+});
+
 const GeoNotes: React.FC = () => {
   const { notes, checkIn, deleteNote } = useGeoNotes();
   const mapRef = useRef<LeafletMap | null>(null);
